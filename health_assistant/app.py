00ccf184173341130b563631988a921d30eed6e2
Goutham523 <gouthamktm005@gmail.com>
2026-02-10 15:15:20 +0530

    Updated

--- health_assistant/app.py
+++ health_assistant/app.py
@@ -2,9 +2,11 @@ import streamlit as st
 from src.ui_core.chats_view import render_chat
 from pages.update_prompts import render_update_prompts
 
-st.set_page_config(page_title="Healthcare AI Admin", layout="wide")
+st.set_page_config(page_title="Healthcare AI Admin Dashboard", layout="wide")
 
-# Create tabs for different functionalities
+st.title("Healthcare AI ChatBot Dashboard")
+st.caption("Chat management and prompt updation")
+
 tab1, tab2 = st.tabs(["Chat", "Update Prompts"])
 
 with tab1:
